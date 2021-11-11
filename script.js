a1e7816acc4629456358000f3a212a93adaccd17
Faruq <hurraf2006@gmail.com>
2021-11-11 15:22:18 +0500

    bowqattan

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
 var first = prompt("Birinchi sonni kiriting: ")
 var second = prompt("Ikkinchi sonni kiriting: ")
 var third = prompt("Uchinchi sonni kiriting")
-if( first>second && first<third) {
+if( first>second && first<third || first>third && first<second) {
     alert("Qiymati o'rtancha son bu: " + first)
 }
 
-else if(first>third && first<second) {
-    alert("Qiymati o'rtancha son bu: " + first)
-}
 
-else if(second>first && second<third){
+
+else if(second>first && second<third || second>third && second<first){
     alert("Qiymati o'rtancha son bu: " + second)
 }
 
-else if(second>third && second<first){
-    alert("Qiymati o'rtancha son bu: " + second)
-}
 
-else if(third>first && third<second){
+
+else if(third>first && third<second || third>second && third<first){
     alert("Qiymati o'rtancha son bu: " + third)
 }
 
-else if(third>second && third<first){
-    alert("Qiymati o'rtancha son bu: " + third)
-}
+
 
 else{
     alert('Batafsil malumot uchun "ok" tugmasini bosing')
